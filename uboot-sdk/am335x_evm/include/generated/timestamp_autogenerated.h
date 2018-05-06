686a2289bdd486afd069b3c28aab2a5272cd0e3d
AppleCai <cailing_07@126.com>
2018-05-06 20:03:16 +0800

    build uboot success

---
+++ uboot-sdk/am335x_evm/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,4 @@
+#define U_BOOT_DATE "May 06 2018"
+#define U_BOOT_TIME "20:00:20"
+#define U_BOOT_TZ "+0800"
+#define U_BOOT_DMI_DATE "05/06/2018"
---
+++ uboot-sdk/am335x_evm/include/generated/version_autogenerated.h
@@ -0,0 +1,4 @@
+#define PLAIN_VERSION "2017.01"
+#define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
+#define CC_VERSION_STRING "arm-linux-gnueabihf-gcc (Ubuntu/Linaro 4.8.2-16ubuntu4) 4.8.2"
+#define LD_VERSION_STRING "GNU ld (GNU Binutils for Ubuntu) 2.24"
