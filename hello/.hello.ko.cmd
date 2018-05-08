fcd41b0fb9f5f7575fae8aacf6683cb1fc2a11c7
AppleCai <cailing_07@126.com>
2018-05-08 07:14:24 -0700

    remove hello folder

--- hello/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/study/test/board-support/hello/hello.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/study/test/board-support/hello/hello.ko /home/study/test/board-support/hello/hello.o /home/study/test/board-support/hello/hello.mod.o
--- hello/.tmp_versions/hello.mod
+++
@@ -1,2 +0,0 @@
-/home/study/test/board-support/hello/hello.ko
-/home/study/test/board-support/hello/hello.o
